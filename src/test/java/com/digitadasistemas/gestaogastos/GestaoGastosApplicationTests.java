9cf5c9311441c4db33a2dd17d6f565a78e18e894
wagnerlemos94 <wagner.lemos94@hotmail.com>
2022-03-03 22:01:42 -0300

    Projeto Criado

---
+++ src/test/java/com/digitadasistemas/gestaogastos/GestaoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitadasistemas.gestaogastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
